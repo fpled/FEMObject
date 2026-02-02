6937161725c9e9c8643a5306648ce733b0316b31
Florent Pled <florent.pled@univ-eiffel.fr>
2026-02-02 18:48:20 +0100

    Update functions randAnisotElasField and randAnisotElasMatrix by adding comments

--- BASIC/TOOLS/randAnisotElasField.m
+++ BASIC/TOOLS/randAnisotElasField.m
@@ -3,14 +3,11 @@ function C = randAnisotElasField(delta,mL,V)
 % delta : scalar dispersion parameter between 0 and delta_sup =
 % sqrt((n3D+1)/(n3D+5)) = sqrt(7/11) = 0.7977 for n3D = 6
 % controlling the level of statistical fluctuations of random field G
-% mL : n-by-n upper triangular matrix of the Cholesky factor of n-by-n mean
-% elasticity matrix mC, or n-by-n-by-nx array containing n-by-n mean
-% elasticity matrix mC evaluated at nx points, with n=3 in 2D and n=6 in 3D
-% V : n(n+1)/2-by-N-by-nx array containing N samples of centered Gaussian random
-% vector gathering the nU=n(n+1)/2 normalized Gaussian real-valued random
-% fields U_j, j=1,...,n(n+1)/2, evaluated at nx points
-% C : n-by-n-by-N-by-nx array containing N samples of n-by-n non-Gaussian
-% random elasticity matrix C evaluated at nx points
+% mL : n-by-n upper triangular matrix corresponding to the Cholesky factor of n-by-n mean elasticity matrix mC, 
+% or n-by-n-by-nx array for n-by-n matrix-valued mean elasticity field mC evaluated at nx points, with n=3 in 2D and n=6 in 3D
+% V : n(n+1)/2-by-N-by-nx array containing N samples of centered Gaussian random vector 
+% gathering the nU=n(n+1)/2 normalized Gaussian real-valued random fields U_j, j=1,...,n(n+1)/2, evaluated at nx points
+% C : n-by-n-by-N-by-nx array containing N samples of n-by-n matrix-valued non-Gaussian random elasticity field C evaluated at nx points
 
 [nU,N,nx] = size(V);
 n3D = 6;
@@ -25,6 +22,7 @@ switch nU
         error('Wrong size for random elasticity field')
 end
 
+%% Gaussian stochastic germs for Gaussian (real-valued) and Gamma (positive-valued) random fields
 VGauss = triu(ones(n),1);
 VGauss = repmat(VGauss,1,1,N*nx);
 VGauss(VGauss==1) = V(1:n*(n-1)/2,:,:);
@@ -34,12 +32,13 @@ VGamma = repmat(VGamma,1,1,N*nx);
 VGamma(VGamma==1) = V(n*(n-1)/2+1:n*(n+1)/2,:,:);
 clear V
 
+%% Parameters
 sigma = delta/sqrt(n3D+1);
 alpha = 1/(2*sigma^2) + (1-(1:n)')/2;
 alpha = diag(alpha);
 beta = eye(n); % beta = diag(ones(n,1));
 
-%% Parallel version (lesser efficiency)
+%% Parallel (parfor loop) implementation for simulating Gamma (positive-valued) random fields Gam (lesser efficiency)
 % Gam = zeros(size(VGamma));
 % if ~verLessThan('matlab','9.2') % introduced in R2017a
 %     q = parallel.pool.DataQueue;
@@ -57,13 +56,13 @@ beta = eye(n); % beta = diag(ones(n,1));
 % end
 % textprogressbar(' done');
 
-%% Vectorized version (faster efficiency)
+%% Vectorized implementation for simulating Gamma (positive-valued) random fields Gam (faster efficiency)
 alpha = repmat(alpha,1,1,N*nx);
 beta = repmat(beta,1,1,N*nx);
 Gam = gaminv(normcdf(VGamma),alpha,beta);
-
 clear VGamma
 
+%% Upper triangular matrix-valued random field L and associated transpose Lt
 LGauss = sigma * VGauss;
 LGamma = sigma * sqrt(2*Gam);
 clear VGauss Gam alpha beta
@@ -71,6 +70,7 @@ L = LGauss + LGamma;
 clear LGauss LGamma
 Lt = permute(L,[2,1,3]);
 
+%% Upper triangular matrix or matrix-valued field mL (Cholesky factor of mean elasticity matrix or matrix-valued field mC) and associated transpose mLt 
 if ismatrix(mL)
     mLt = mL';
 else
@@ -78,7 +78,73 @@ else
     mLt = permute(mL,[2,1,3,4]);
 end
 
-%% Tianyu's vectorized version
+%% Sequential (for loop) implementation for simulating non-Gaussian matrix-valued random elasticity field C (lesser efficiency)
+% C = zeros(n,n,N*nx);
+% for k=1:N*nx
+%     G = L(:,:,k)'*L(:,:,k);
+%     if ismatrix(mL)
+%         C(:,:,k) = mL'*G*mL;
+%     else
+%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
+%     end
+% end
+
+%% Parallel (parfor loop) implementation for simulating non-Gaussian matrix-valued random elasticity field C (worst efficiency)
+% C = zeros(n,n,N*nx);
+% if ~verLessThan('matlab','9.2') % introduced in R2017a
+%     q = parallel.pool.DataQueue;
+%     afterEach(q, @nUpdateProgressBar);
+% else
+%     q = [];
+% end
+% textprogressbar('Computing elasticity random fields: ');
+% p = 0;
+% parfor k=1:N*nx
+%     if ~verLessThan('matlab','9.2') % introduced in R2017a
+%         send(q,k);
+%     end
+%     G = L(:,:,k)'*L(:,:,k);
+%     if ismatrix(mL)
+%         C(:,:,k) = mL'*G*mL;
+%     else
+%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
+%     end
+% end
+% textprogressbar(' done');
+
+%% Vectorized implementation for simulating non-Gaussian matrix-valued random elasticity field C (faster efficiency)
+% if ismatrix(mL)
+%     mLt = repmat(mLt,1,1,N*nx);
+%     mL = repmat(mL,1,1,N*nx);
+% end
+% if verLessThan('matlab','9.1') % compatibility (<R2016b)
+%     G = squeeze(sum(bsxfun(@times,reshape(Lt,[n,n,1,N*nx]),reshape(L,[1,n,n,N*nx])),2));
+%     mLtG = squeeze(sum(bsxfun(@times,reshape(mLt,[n,n,1,N*nx]),reshape(G,[1,n,n,N*nx])),2));
+%     C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N*nx]),reshape(mL,[1,n,n,N*nx])),2);
+% else
+%     G = sum(reshape(Lt,[n,n,1,N*nx]).*reshape(L,[1,n,n,N*nx]),2);
+%     mLtG = sum(reshape(mLt,[n,n,1,N*nx]).*reshape(G,[1,n,n,N*nx]),2);
+%     C = sum(reshape(mLtG,[n,n,1,N*nx]).*reshape(mL,[1,n,n,N*nx]),2);
+% end
+
+if ismatrix(mL)
+    mLt = reshape(mLt,[n,n,1]);
+    mL = reshape(mL,[1,n,n]);
+else
+    mLt = reshape(mLt,[n,n,1,N*nx]);
+    mL = reshape(mL,[1,n,n,N*nx]);
+end
+if verLessThan('matlab','9.1') % compatibility (<R2016b)
+    G = sum(bsxfun(@times,reshape(Lt,[n,n,1,N*nx]),reshape(L,[1,n,n,N*nx])),2);
+    mLtG = sum(bsxfun(@times,mLt,reshape(G,[1,n,n,N*nx])),2);
+    C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N*nx]),mL),2);
+else
+    G = sum(reshape(Lt,[n,n,1,N*nx]).*reshape(L,[1,n,n,N*nx]),2);
+    mLtG = sum(mLt.*reshape(G,[1,n,n,N*nx]),2);
+    C = sum(reshape(mLtG,[n,n,1,N*nx]).*mL,2);
+end
+
+%% Tianyu Zhang's vectorized implementation for simulating non-Gaussian matrix-valued random elasticity field C
 % aa = 1:n*N*nx;
 % ai = reshape(aa,n,N*nx);
 % bi = repmat(ai,n,1);
@@ -114,74 +180,10 @@ end
 % CMat = BPIMat * mLPrmMat;
 % C = permute(reshape(CMat,n,N*nx,n),[1,3,2]);
 
-%% Sequential (for loop) version (lesser efficiency)
-% C = zeros(n,n,N*nx);
-% for k=1:N*nx
-%     G = L(:,:,k)'*L(:,:,k);
-%     if ismatrix(mL)
-%         C(:,:,k) = mL'*G*mL;
-%     else
-%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
-%     end
-% end
-
-%% Parallel (parfor loop) version (worst efficiency)
-% C = zeros(n,n,N*nx);
-% if ~verLessThan('matlab','9.2') % introduced in R2017a
-%     q = parallel.pool.DataQueue;
-%     afterEach(q, @nUpdateProgressBar);
-% else
-%     q = [];
-% end
-% textprogressbar('Computing elasticity random fields: ');
-% p = 0;
-% parfor k=1:N*nx
-%     if ~verLessThan('matlab','9.2') % introduced in R2017a
-%         send(q,k);
-%     end
-%     G = L(:,:,k)'*L(:,:,k);
-%     if ismatrix(mL)
-%         C(:,:,k) = mL'*G*mL;
-%     else
-%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
-%     end
-% end
-% textprogressbar(' done');
-
-%% Flo's vectorized version (faster efficiency)
-% if ismatrix(mL)
-%     mLt = repmat(mLt,1,1,N*nx);
-%     mL = repmat(mL,1,1,N*nx);
-% end
-% if verLessThan('matlab','9.1') % compatibility (<R2016b)
-%     G = squeeze(sum(bsxfun(@times,reshape(Lt,[n,n,1,N*nx]),reshape(L,[1,n,n,N*nx])),2));
-%     mLtG = squeeze(sum(bsxfun(@times,reshape(mLt,[n,n,1,N*nx]),reshape(G,[1,n,n,N*nx])),2));
-%     C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N*nx]),reshape(mL,[1,n,n,N*nx])),2);
-% else
-%     G = sum(reshape(Lt,[n,n,1,N*nx]).*reshape(L,[1,n,n,N*nx]),2);
-%     mLtG = sum(reshape(mLt,[n,n,1,N*nx]).*reshape(G,[1,n,n,N*nx]),2);
-%     C = sum(reshape(mLtG,[n,n,1,N*nx]).*reshape(mL,[1,n,n,N*nx]),2);
-% end
-
-if ismatrix(mL)
-    mLt = reshape(mLt,[n,n,1]);
-    mL = reshape(mL,[1,n,n]);
-else
-    mLt = reshape(mLt,[n,n,1,N*nx]);
-    mL = reshape(mL,[1,n,n,N*nx]);
-end
-if verLessThan('matlab','9.1') % compatibility (<R2016b)
-    G = sum(bsxfun(@times,reshape(Lt,[n,n,1,N*nx]),reshape(L,[1,n,n,N*nx])),2);
-    mLtG = sum(bsxfun(@times,mLt,reshape(G,[1,n,n,N*nx])),2);
-    C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N*nx]),mL),2);
-else
-    G = sum(reshape(Lt,[n,n,1,N*nx]).*reshape(L,[1,n,n,N*nx]),2);
-    mLtG = sum(mLt.*reshape(G,[1,n,n,N*nx]),2);
-    C = sum(reshape(mLtG,[n,n,1,N*nx]).*mL,2);
-end
-
+%% Final reshape
 C = reshape(C,n,n,N,nx);
 
+%% Text progress bar for parallel implementation
 % function nUpdateProgressBar(~)
 % p = p+1;
 % textprogressbar(p/(N*nx)*100,sprintf('(%d/%d)',p,N*nx));
--- BASIC/TOOLS/randAnisotElasMatrix.m
+++ BASIC/TOOLS/randAnisotElasMatrix.m
@@ -3,10 +3,10 @@ function C = randAnisotElasMatrix(delta,mL,V)
 % delta : scalar dispersion parameter between 0 and delta_sup =
 % sqrt((n3D+1)/(n3D+5)) = sqrt(7/11) = 0.7977 for n3D = 6
 % controlling the level of statistical fluctuations of random matrix G
-% mL : n-by-n upper triangular matrix of the Cholesky factor of n-by-n mean
-% elasticity matrix mC with n=3 in 2D and n=6 in 3D
-% V : n(n+1)/2-by-N matrix containing N samples of centered Gaussian random
-% vector gathering the n(n+1)/2 normalized Gaussian real-valued random variables U_j, j=1,...,n(n+1)/2
+% mL : n-by-n upper triangular matrix corresponding to the Cholesky factor of n-by-n mean elasticity matrix mC, 
+% with n=3 in 2D and n=6 in 3D
+% V : n(n+1)/2-by-N matrix containing N samples of centered Gaussian random vector 
+% gathering the nU=n(n+1)/2 normalized Gaussian real-valued random variables U_j, j=1,...,n(n+1)/2
 % C : n-by-n-by-N array containing N samples of n-by-n non-Gaussian random elasticity matrix C
 
 N = size(V,2);
@@ -22,6 +22,7 @@ switch size(V,1)
         error('Wrong size for random elasticity matrix')
 end
 
+%% Gaussian stochastic germs for Gaussian (real-valued) and Gamma (positive-valued) random variables
 VGauss = triu(ones(n),1);
 VGauss = repmat(VGauss,1,1,N);
 VGauss(VGauss==1) = V(1:n*(n-1)/2,:);
@@ -31,12 +32,13 @@ VGamma = repmat(VGamma,1,1,N);
 VGamma(VGamma==1) = V(n*(n-1)/2+1:n*(n+1)/2,:);
 clear V
 
+%% Parameters
 sigma = delta/sqrt(n3D+1);
 alpha = 1/(2*sigma^2) + (1-(1:n)')/2;
 alpha = diag(alpha);
 beta = eye(n); % beta = diag(ones(n,1));
 
-%% Parallel version (lesser efficiency)
+%% Parallel (parfor loop) implementation for simulating Gamma (positive-valued) random variables Gam (lesser efficiency)
 % Gam = zeros(size(VGamma));
 % if ~verLessThan('matlab','9.2') % introduced in R2017a
 %     q = parallel.pool.DataQueue;
@@ -54,12 +56,13 @@ beta = eye(n); % beta = diag(ones(n,1));
 % end
 % textprogressbar(' done');
 
-%% Vectorized version (faster efficiency)
+%% Vectorized implementation for simulating Gamma (positive-valued) random variables Gam (faster efficiency)
 alpha = repmat(alpha,1,1,N);
 beta = repmat(beta,1,1,N);
 Gam = gaminv(normcdf(VGamma),alpha,beta);
 clear VGamma
 
+%% Upper triangular random matrix L and associated transpose Lt
 LGauss = sigma * VGauss;
 LGamma = sigma * sqrt(2*Gam);
 clear VGauss Gam alpha beta
@@ -67,13 +70,80 @@ L = LGauss + LGamma;
 clear LGauss LGamma
 Lt = permute(L,[2,1,3]);
 
+%% Upper triangular matrix mL (Cholesky factor of mean elasticity matrix mC) and associated transpose mLt 
 if ismatrix(mL)
     mLt = mL';
 else
     mLt = permute(mL,[2,1,3]);
 end
 
-%% Tianyu's vectorized version
+%% Sequential (for loop) implementation for simulating non-Gaussian random elasticity matrix C (lesser efficiency)
+% C = zeros(n,n,N);
+% for k=1:N
+%     G = L(:,:,k)'*L(:,:,k);
+%     if ismatrix(mL)
+%         C(:,:,k) = mL'*G*mL;
+%     else
+%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
+%     end
+% end
+
+%% Parallel (parfor loop) implementation for simulating non-Gaussian random elasticity matrix C (worst efficiency)
+% C = zeros(n,n,N);
+% if ~verLessThan('matlab','9.2') % introduced in R2017a
+%     q = parallel.pool.DataQueue;
+%     afterEach(q, @nUpdateProgressBar);
+% else
+%     q = [];
+% end
+% textprogressbar('Computing elasticity random fields: ');
+% p = 0;
+% parfor k=1:N
+%     if ~verLessThan('matlab','9.2') % introduced in R2017a
+%         send(q,k);
+%     end
+%     G = L(:,:,k)'*L(:,:,k);
+%     if ismatrix(mL)
+%         C(:,:,k) = mL'*G*mL;
+%     else
+%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
+%     end
+% end
+% textprogressbar(' done');
+
+%% Vectorized implementation for simulating non-Gaussian random elasticity matrix C (faster efficiency)
+% if ismatrix(mL)
+%     mLt = repmat(mLt,1,1,N);
+%     mL = repmat(mL,1,1,N);
+% end
+% if verLessThan('matlab','9.1') % compatibility (<R2016b)
+%     G = squeeze(sum(bsxfun(@times,reshape(Lt,[n,n,1,N]),reshape(L,[1,n,n,N])),2));
+%     mLtG = squeeze(sum(bsxfun(@times,reshape(mLt,[n,n,1,N]),reshape(G,[1,n,n,N])),2));
+%     C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N]),reshape(mL,[1,n,n,N])),2);
+% else
+%     G = sum(reshape(Lt,[n,n,1,N]).*reshape(L,[1,n,n,N]),2);
+%     mLtG = sum(reshape(mLt,[n,n,1,N]).*reshape(G,[1,n,n,N]),2);
+%     C = sum(reshape(mLtG,[n,n,1,N]).*reshape(mL,[1,n,n,N]),2);
+% end
+
+if ismatrix(mL)
+    mLt = reshape(mLt,[n,n,1]);
+    mL = reshape(mL,[1,n,n]);
+else
+    mLt = reshape(mLt,[n,n,1,N]);
+    mL = reshape(mL,[1,n,n,N]);
+end
+if verLessThan('matlab','9.1') % compatibility (<R2016b)
+    G = sum(bsxfun(@times,reshape(Lt,[n,n,1,N]),reshape(L,[1,n,n,N])),2);
+    mLtG = sum(bsxfun(@times,mLt,reshape(G,[1,n,n,N])),2);
+    C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N]),mL),2);
+else
+    G = sum(reshape(Lt,[n,n,1,N]).*reshape(L,[1,n,n,N]),2);
+    mLtG = sum(mLt.*reshape(G,[1,n,n,N]),2);
+    C = sum(reshape(mLtG,[n,n,1,N]).*mL,2);
+end
+
+%% Tianyu Zhang's vectorized implementation for simulating non-Gaussian random elasticity matrix C
 % aa = 1:n*N;
 % ai = reshape(aa,n,N);
 % bi = repmat(ai,n,1);
@@ -109,74 +179,10 @@ end
 % CMat = BPIMat * mLPrmMat;
 % C = permute(reshape(CMat,n,N,n),[1,3,2]);
 
-%% Sequential (for loop) version (lesser efficiency)
-% C = zeros(n,n,N);
-% for k=1:N
-%     G = L(:,:,k)'*L(:,:,k);
-%     if ismatrix(mL)
-%         C(:,:,k) = mL'*G*mL;
-%     else
-%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
-%     end
-% end
-
-%% Parallel (parfor loop) version (worst efficiency)
-% C = zeros(n,n,N);
-% if ~verLessThan('matlab','9.2') % introduced in R2017a
-%     q = parallel.pool.DataQueue;
-%     afterEach(q, @nUpdateProgressBar);
-% else
-%     q = [];
-% end
-% textprogressbar('Computing elasticity random fields: ');
-% p = 0;
-% parfor k=1:N
-%     if ~verLessThan('matlab','9.2') % introduced in R2017a
-%         send(q,k);
-%     end
-%     G = L(:,:,k)'*L(:,:,k);
-%     if ismatrix(mL)
-%         C(:,:,k) = mL'*G*mL;
-%     else
-%         C(:,:,k) = mL(:,:,k)'*G*mL(:,:,k);
-%     end
-% end
-% textprogressbar(' done');
-
-%% Flo's vectorized version (faster efficiency)
-% if ismatrix(mL)
-%     mLt = repmat(mLt,1,1,N);
-%     mL = repmat(mL,1,1,N);
-% end
-% if verLessThan('matlab','9.1') % compatibility (<R2016b)
-%     G = squeeze(sum(bsxfun(@times,reshape(Lt,[n,n,1,N]),reshape(L,[1,n,n,N])),2));
-%     mLtG = squeeze(sum(bsxfun(@times,reshape(mLt,[n,n,1,N]),reshape(G,[1,n,n,N])),2));
-%     C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N]),reshape(mL,[1,n,n,N])),2);
-% else
-%     G = sum(reshape(Lt,[n,n,1,N]).*reshape(L,[1,n,n,N]),2);
-%     mLtG = sum(reshape(mLt,[n,n,1,N]).*reshape(G,[1,n,n,N]),2);
-%     C = sum(reshape(mLtG,[n,n,1,N]).*reshape(mL,[1,n,n,N]),2);
-% end
-
-if ismatrix(mL)
-    mLt = reshape(mLt,[n,n,1]);
-    mL = reshape(mL,[1,n,n]);
-else
-    mLt = reshape(mLt,[n,n,1,N]);
-    mL = reshape(mL,[1,n,n,N]);
-end
-if verLessThan('matlab','9.1') % compatibility (<R2016b)
-    G = sum(bsxfun(@times,reshape(Lt,[n,n,1,N]),reshape(L,[1,n,n,N])),2);
-    mLtG = sum(bsxfun(@times,mLt,reshape(G,[1,n,n,N])),2);
-    C = sum(bsxfun(@times,reshape(mLtG,[n,n,1,N]),mL),2);
-else
-    G = sum(reshape(Lt,[n,n,1,N]).*reshape(L,[1,n,n,N]),2);
-    mLtG = sum(mLt.*reshape(G,[1,n,n,N]),2);
-    C = sum(reshape(mLtG,[n,n,1,N]).*mL,2);
-end
-
+%% Final reshape
 C = reshape(C,n,n,N);
 
+%% Text progress bar for parallel implementation
 % function nUpdateProgressBar(~)
 % p = p+1;
 % textprogressbar(p/N*100,sprintf('(%d/%d)',p,N));
